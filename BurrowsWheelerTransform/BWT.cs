43a7dc2f8b8dafcdc905397b3b5af86ce25fb452
JarousNemec <jarous.nemec@gmail.com>
2023-03-23 22:19:02 +0100

    Initial commit

---
+++ BurrowsWheelerTransform/BWT.cs
@@ -0,0 +1,54 @@
+﻿namespace BurrowsWheelerTransform;
+
+public class BWT
+{
+    public string Transform(string input)
+    {
+        string output = String.Empty;
+        var allRotations = MakeAllRotations(input);
+        return output;
+    }
+
+    private string[] MakeAllRotations(string input)
+    {
+        var rotations = new string[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            rotations[i] = input;
+            for (int j = 0; j < i; j++)
+            {
+               rotations[i] = Rotate(rotations[i]);
+            } 
+        }
+
+        return rotations;
+    }
+
+    private string Rotate(string input)
+    {
+        
+        char[] output = input.ToCharArray();
+        
+        char replacer = output[0];
+        char temp;
+        for (int i = 0; i < output.Length; i++)
+        {
+            if (i == output.Length-1)
+            {
+                output[0] = replacer;
+                break;
+            }
+            temp = output[i + 1];
+            output[i+1] = replacer;
+            replacer = temp;
+        }
+        return new string(output);
+    }
+
+    public string DeTransform(string input)
+    {
+        string output = String.Empty;
+
+        return output;
+    }
+}
---
+++ BurrowsWheelerTransform/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BurrowsWheelerTransform;
+
+Console.WriteLine("Hello, World!");
+var transformer = new BWT();
+var toTransform = "ananas";
+var toDetransform = transformer.Transform(toTransform);
+var detransformed = transformer.DeTransform(toDetransform);
+
+Console.WriteLine(toTransform);
+Console.WriteLine(toDetransform);
+Console.WriteLine(detransformed);
