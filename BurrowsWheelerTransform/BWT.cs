fa639b4bba5c1388c92d5b8e92c5bbf284c8b121
JarousNemec <jarous.nemec@gmail.com>
2023-03-24 14:27:19 +0100

    working on sorter

--- BurrowsWheelerTransform/BWT.cs
+++ BurrowsWheelerTransform/BWT.cs
@@ -5,20 +5,79 @@ public class BWT
     public string Transform(string input)
     {
         string output = String.Empty;
-        var allRotations = MakeAllRotations(input);
+        var data = input.ToLower();
+        var allRotations = MakeAllRotations(data);
         var sorted = LexographicsSort(allRotations);
+        foreach (var rotation in sorted)
+        {
+            output += rotation[^1];
+        }
+
         return output;
     }
 
     private string[] LexographicsSort(string[] toSort)
     {
-        var sorted = toSort;
-        
-        
-        
-        return sorted;
+        var data = toSort;
+        char minChar = char.MaxValue;
+        var selectedIndex = 0;
+        List<int[]> areas = new List<int[]>();
+        char actualSortedChar = Char.MaxValue;
+        int areaStart = 0;
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            for (int l = 0; l < areas.Count; l++)
+            {
+                for (int j = 0; j < data.Length; j++)
+                {
+                    for (int k = j; k < data.Length; k++)
+                    {
+                        if (data[k][i] <= minChar)
+                        {
+                            selectedIndex = k;
+                            minChar = data[k][i];
+                        }
+                    }
+
+                    minChar = char.MaxValue;
+
+                    if (actualSortedChar != char.MaxValue)
+                    {
+                        if (actualSortedChar != data[selectedIndex][i])
+                        {
+                            areas.Add(new []{areaStart, j});
+                            areaStart = j;
+                        }
+                    }
+                    else
+                    {
+                        actualSortedChar = data[selectedIndex][i];
+                    }
+
+                    if (ArePreviousLettersSame(data, j, selectedIndex, i))
+                        (data[j], data[selectedIndex]) = (data[selectedIndex], data[j]);
+                }
+
+                selectedIndex = 0;
+            }
+
+            areas.Clear();
+        }
+
+        return data;
     }
-    
+
+    private bool ArePreviousLettersSame(string[] data, int baseIndex, int selectedIndex, int level)
+    {
+        for (int i = 0; i < level; i++)
+        {
+            if (data[baseIndex][i] != data[selectedIndex][i])
+                return false;
+        }
+
+        return true;
+    }
+
 
     private string[] MakeAllRotations(string input)
     {
@@ -28,8 +87,8 @@ public class BWT
             rotations[i] = input;
             for (int j = 0; j < i; j++)
             {
-               rotations[i] = Rotate(rotations[i]);
-            } 
+                rotations[i] = Rotate(rotations[i]);
+            }
         }
 
         return rotations;
@@ -42,15 +101,17 @@ public class BWT
         char temp;
         for (int i = 0; i < output.Length; i++)
         {
-            if (i == output.Length-1)
+            if (i == output.Length - 1)
             {
                 output[0] = replacer;
                 break;
             }
+
             temp = output[i + 1];
-            output[i+1] = replacer;
+            output[i + 1] = replacer;
             replacer = temp;
         }
+
         return new string(output);
     }
 
